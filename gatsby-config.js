a1b61a3b970879b60249752ca511c8b0dd1bb075
ian knauer <ian.knauer@gmail.com>
2018-10-26 13:50:03 -0700

    added google analytics tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,24 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter Tailwind',
+    title: 'Davos | Give Meaning',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+     resolve: `gatsby-plugin-google-analytics`,
+     options: {
+       trackingId: "UA-128187880-1",
+       // Puts tracking script in the head instead of the body
+       head: false,
+       // Setting this parameter is optional
+       anonymize: true,
+       // Setting this parameter is also optional
+       respectDNT: true,
+       // Avoids sending pageview hits from custom paths
+       // Enables Google Optimize using your container Id
+       // Any additional create only fields (optional)
+
+     },
+   },
+  ],
 };
